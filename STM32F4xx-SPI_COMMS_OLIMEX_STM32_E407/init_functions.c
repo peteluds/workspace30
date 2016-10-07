789c5a8bbd10c0eb5f85e3e91f51651315757f39
peteluds <peter.ludlow@arralis.com>
2016-10-07 11:16:50 +0100

    Peter's changes

--- STM32F4xx-SPI_COMMS_OLIMEX_STM32_E407/init_functions.c
+++ STM32F4xx-SPI_COMMS_OLIMEX_STM32_E407/init_functions.c
@@ -37,15 +37,29 @@ static const SPIConfig hs_spicfg = {
  * ADF4159/ADF4355 Frequency Synthesizer Register Values
  */
 
+static uint8_t ADF4159_power_on_register_values_buf[11][4] = {
+{0x00,0x00,0x00,0x07}, // Write to ADF4159 register 7
+{0x00,0x00,0x3E,0x86}, // Write to ADF4159 register 6 [with STEP SEL = 0]
+{0x00,0x80,0x3E,0x86}, // Write to ADF4159 register 6 [with STEP SEL = 1]
+{0x00,0x12,0x8F,0x75}, // Write to ADF4159 register 5 [with DEV SEL = 0]
+{0x00,0x92,0x8F,0x75}, // Write to ADF4159 register 5 [with DEV SEL = 1]
+{0x00,0x18,0x00,0x84}, // Write to ADF4159 register 4 [with CLK DIV SEL = 0]
+{0x00,0x18,0x00,0xC4}, // Write to ADF4159 register 4 [with CLK DIV SEL = 1]
+{0x00,0x63,0x04,0xC3}, // Write to ADF4159 register 3
+{0x10,0x40,0x01,0x92}, // Write to ADF4159 register 2
+{0x00,0x00,0x00,0x01}, // Write to ADF4159 register 1
+{0xB0,0x36,0x60,0x00}, // Write to ADF4159 register 0
+};
+
 static uint8_t ADF4159_register_values_buf[8][4] = {
-{0x34,0x34,0x34,0x34}, // Write to ADF4159 register 0
-{0x34,0x34,0x34,0x34}, // Write to ADF4159 register 1
-{0x34,0x34,0x34,0x34}, // Write to ADF4159 register 2
-{0x34,0x34,0x34,0x34}, // Write to ADF4159 register 3
-{0x34,0x34,0x34,0x34}, // Write to ADF4159 register 4
-{0x34,0x34,0x34,0x34}, // Write to ADF4159 register 5
-{0x34,0x34,0x34,0x34}, // Write to ADF4159 register 6
-{0x34,0x34,0x34,0x34}, // Write to ADF4159 register 7
+{0x00,0x00,0x00,0x07}, // Write to ADF4159 register 7
+{0x00,0x00,0x3E,0x86}, // Write to ADF4159 register 6
+{0x00,0x12,0x8F,0x75}, // Write to ADF4159 register 5
+{0x00,0x18,0x00,0x84}, // Write to ADF4159 register 4
+{0x00,0x63,0x04,0xC3}, // Write to ADF4159 register 3
+{0x10,0x40,0x01,0x92}, // Write to ADF4159 register 2
+{0x00,0x00,0x00,0x01}, // Write to ADF4159 register 1
+{0xB0,0x36,0x60,0x00}, // Write to ADF4159 register 0
 };
 
 static uint8_t ADF4355_register_values_buf[13][4] = {
@@ -71,6 +85,32 @@ static uint8_t ADF4355_register_values_buf[13][4] = {
 void ADF4159_init(void){
 
     int i;
+
+    /*
+     * Program ADF4159 with power-on register values, i.e. load registers from 7-0, load registers 6/5/4 twice
+     */
+
+    spiAcquireBus(&SPID1);
+    spiStart(&SPID1, &hs_spicfg);
+
+    //Select multiplexed line
+    palClearPad(GPIOG, GPIOG_SPI_NSS_S0);
+    palClearPad(GPIOG, GPIOG_SPI_NSS_S1);
+
+    // Configure ADF4159 registers
+    for(i = 0; i < 11; i++){
+        spiSelect(&SPID1);
+        spiSend(&SPID1,11,ADF4159_power_on_register_values_buf[i]);
+        spiUnselect(&SPID1);
+    }
+
+    //chThdSleepMilliseconds(200);
+    spiStop(&SPID1);
+    spiReleaseBus(&SPID1);
+
+    /*
+     * Program ADF4159 with desired register values
+     */
 
     spiAcquireBus(&SPID1);
     spiStart(&SPID1, &hs_spicfg);
@@ -116,4 +156,3 @@ void ADF4355_init(void){
     spiReleaseBus(&SPID1);
 
 }
-
